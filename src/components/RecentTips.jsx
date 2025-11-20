6b40cabc7206d32f0e37e345d98a39f574959c2b
imadisimran <imadisimran@gmail.com>
2025-11-20 17:07:12 +0600

    Recent Tips

---
+++ src/components/RecentTips.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+import RecentTipsCard from './RecentTipsCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation } from 'swiper/modules';
+
+
+const RecentTips = () => {
+    return (
+        <section className='w-11/12 mx-auto'>
+            <StyledWrapper>
+                <div className="cards">
+                    <Swiper
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                            pauseOnMouseEnter: true
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Autoplay,Navigation]}
+
+                        spaceBetween={10}
+                        // Default (Mobile / < 640px): 1 Slide
+                        slidesPerView={1}
+                        breakpoints={{
+                            // Tailwind 'sm' (640px)
+                            // User: "1 slide per view in small screen"
+                            640: {
+                                slidesPerView: 1,
+                                spaceBetween: 10,
+                            },
+                            // Tailwind 'md' (768px)
+                            // User: "2 in medium"
+                            768: {
+                                slidesPerView: 2,
+                                spaceBetween: 10,
+                            },
+                            // Tailwind 'lg' (1024px)
+                            // User: "3 in large"
+                            1024: {
+                                slidesPerView: 3,
+                                spaceBetween: 10,
+                            },
+                        }}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide className='p-5'>
+                            <div className="card">
+                                <RecentTipsCard></RecentTipsCard>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className='p-5'>
+                            <div className="card">
+                                <RecentTipsCard></RecentTipsCard>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className='p-5'>
+                            <div className="card">
+                                <RecentTipsCard></RecentTipsCard>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className='p-5'>
+                            <div className="card">
+                                <RecentTipsCard></RecentTipsCard>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+
+                </div>
+            </StyledWrapper>
+
+        </section>
+    );
+};
+
+
+const StyledWrapper = styled.div`
+  .cards {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .cards .card {
+    // display: flex;
+    // align-items: center;
+    // justify-content: center;
+    // flex-direction: column;
+    // text-align: center;
+    // height: 100px;
+    // width: 250px;
+    // border-radius: 10px;
+    // color: white;
+    cursor: pointer;
+    transition: 400ms;
+  }
+
+
+
+    .cards .card:hover {
+        transform: scale(1.1);
+}
+.cards:has(.card:hover) .card:not(:hover) {
+  filter: blur(10px);
+  transform: scale(0.9);
+}
+
+    `;
+
+export default RecentTips;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import ActiveChallenges from '../components/ActiveChallenges';
+import RecentTips from '../components/RecentTips';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <>
             <Hero></Hero>
             <ActiveChallenges></ActiveChallenges>
+            <RecentTips></RecentTips>
         </>
     );
 };
